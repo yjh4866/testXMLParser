338cb977c2bbbe5ec83bb6ab3f18e1674c7fd7bc
yjh4866 <yjh4866@163.com>
2013-11-01 21:01:02 +0800

    Initial Commit

---
+++ testXMLParser/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  testXMLParser
+//
+//  Created by yangjh on 13-11-1.
+//  Copyright (c) 2013年 yangjh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ testXMLParser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testXMLParser
+//
+//  Created by yangjh on 13-11-1.
+//  Copyright (c) 2013年 yangjh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
